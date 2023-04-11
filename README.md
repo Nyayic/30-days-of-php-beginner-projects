7b9ff396527f327c352cafbcdcc9d90661b916eb
Fanny <34095626+Nyayic@users.noreply.github.com>
2023-04-11 23:31:06 +0300

    day3

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 30 days of php beginner projects
 1. Day 1: create a "Hello World" project
 2. Day 2: Create a program that takes in a user's name and displays a personalized greeting.
+3. Day 3: Rectangle Area and Perimeter Calculator
---
+++ day3/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rectangle: Area and Perimeter Calculator</title>
+</head>
+<body>
+	<h1>Rectangle: Area and Perimeter Calculator</h1>
+	<form method="post">
+		<label for="length">Length:</label>
+		<input type="number" name="length" id="length"><br><br>
+		<label for="width">Width:</label>
+		<input type="number" name="width" id="width"><br><br>
+		<input type="submit" name="submit" value="Calculate">
+	</form>
+
+	<?php 
+	if(isset($_POST['submit'])) {
+		// Retrieve user input
+		$length = $_POST['length'];
+		$width = $_POST['width'];
+
+		// Calculate area and perimeter
+		$area = $length * $width;
+		$perimeter = 2 * ($length + $width);
+
+		// Display the results
+		echo "<br>Length: " . $length;
+		echo "<br>Width: " . $width;
+		echo "<br>Area: " . $area;
+		echo "<br>Perimeter: " . $perimeter;
+	}
+	?>
+</body>
+</html>
